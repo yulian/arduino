dc09d215067f4cbacb3ce034a212675732f5dc5b
yulian <yulian@naver.com>
2024-01-23 13:12:06 +0900

    Create example07-05.ino
    Do it! 07-05 실습 코드 추가

---
+++ examples/example07-05.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+  Serial.begin(9600);             // 시리얼 통신 초기화
+  
+  pinMode(8, OUTPUT);             // 8번 디지털 핀을 출력 모드로 설정
+  pinMode(7, INPUT);              // 7번 디지털 핀을 입력 모드로 설정
+}
+
+void loop()
+{
+  int readValue = digitalRead(7); // 입력 핀의 값을 읽어 변수에 저장
+  Serial.println(readValue);      // 변수의 값을 시리얼 모니터에 출력
+  
+  if(readValue == HIGH) {         // 입력값에 따라 LED 출력값 제어
+    digitalWrite(8, HIGH);
+  }
+  else {
+    digitalWrite(8, LOW);
+  }
+}
