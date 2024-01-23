df955a87bfce213c6d14a1665dc796f455f2a1f5
yulian <yulian@naver.com>
2024-01-23 14:54:35 +0900

    Create example09-06.ino
    Do it! 09-06 실습 코드 추가

---
+++ examples/example09-06.ino
@@ -0,0 +1,27 @@
+void setup()
+{
+  // PWM 지원하는 디지털 9번, 10번 핀을 출력모드로 설정
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  
+  // 스위치의 입력을 받기위해 디지털 8번핀을 입력모드로 설정
+  pinMode(8, INPUT);
+}
+
+void loop()
+{
+  // 가변저항의 입력 값 범위를 map() 함수로 변환
+  int inputValue = analogRead(A0);
+  int convertedValue = map(inputValue, 0, 1023, 0, 255);
+  
+  // 스위치의 입력 값에 따라 DC모터의 방향을 제어
+  int inputSwitch = digitalRead(8);
+  if (inputSwitch == LOW) {
+    analogWrite(9, convertedValue);   // LOW값을 읽으면 9번 핀에 전원을 공급
+    analogWrite(10, 0);
+  }
+  else {
+    analogWrite(9, 0);
+    analogWrite(10, convertedValue);  // LOW값을 읽지 않으면 10번 핀에 전원을 공급
+  }
+}
