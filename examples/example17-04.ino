6aeb6e3a91357d517a382665d0630527e70347cd
yulian <yulian@naver.com>
2024-01-24 17:33:50 +0900

    Create example17-04.ino
    Do it! 17-04 실습 코드 추가

---
+++ examples/example17-04.ino
@@ -0,0 +1,25 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN       2 // 제어를 위한 디지털 핀 번호
+#define NUMPIXELS 1 // 네오픽셀의 LED 수
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // 네오픽셀 라이브러리 초기화
+  pixels.begin();
+}
+
+void loop() {
+  setColor();
+  delay(100);
+}
+
+// 임의의 RGB 색상을 변경하는 함수
+void setColor() {
+  int redColor = random(0, 256);
+  int greenColor = random(0,256);
+  int blueColor = random(0, 256);
+  pixels.setPixelColor(0, pixels.Color(redColor, greenColor, blueColor));
+  pixels.show();
+}
