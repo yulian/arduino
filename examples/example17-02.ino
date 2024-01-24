fbc4996d1706bb92e833d9db595c4f7223a6e2fc
yulian <yulian@naver.com>
2024-01-24 17:24:50 +0900

    Create example17-02.ino
    Do it! 17-02 실습 코드 추가

---
+++ examples/example17-02.ino
@@ -0,0 +1,23 @@
+void setup()
+{
+  for (int i=7; i<=13; i++) { 
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop()
+{
+  for (int i=7; i<=13; i++) { 
+    digitalWrite(i, HIGH);
+    delay(100);
+    digitalWrite(i, LOW);
+    delay(100);
+  }
+
+  for (int i=12; i>7; i--) { 
+    digitalWrite(i, HIGH);
+    delay(100);
+    digitalWrite(i, LOW);
+    delay(100);
+  }
+}
