6dd42f7801d951273872cbce9599116217f6ab1d
yulian <yulian@naver.com>
2024-01-24 10:26:47 +0900

    Create example13-01.ino
    Do it! 13-01 실습 코드 추가

---
+++ examples/example13-01.ino
@@ -0,0 +1,22 @@
+#include <IRremote.h>
+#define IR_RECEIVE_PIN 8  // 적외선 센서가 연결된 디지털 핀 번호
+
+void setup() {
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+  pinMode(9, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  if (IrReceiver.decode()) {  // 적외선 센서의 수신값 해석
+    Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+    unsigned long data = IrReceiver.decodedIRData.decodedRawData;
+    if (data == 0xF30CBF00)   // 적외선 리모컨의 숫자 0을 누를 때 수신하는 값
+      digitalWrite(9, HIGH);
+  else
+      digitalWrite(9, LOW);
+  delay(30);
+  IrReceiver.resume();
+  }
+}
